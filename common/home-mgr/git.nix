0655f9cda559fe3544d3e1c71baaaf16f3b983bc
ru322 <168622173+ru322@users.noreply.github.com>
2026-04-03 17:35:37 +0900

    とりあえず日本語入力できた

--- common/home-mgr/git.nix
+++ common/home-mgr/git.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 {
+  programs.git.enable = true;
   programs.git.settings = {
-    enable = true;
-    userName = "ru322";
-    userEmail = "168622173+ru322@users.noreply.github.com";
+    #enable = true;
+    user.name = "ru322";
+    user.email = "168622173+ru322@users.noreply.github.com";
   };
 }
--- common/nixos/fcitx5.nix
+++ common/nixos/fcitx5.nix
@@ -5,6 +5,16 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = [ pkgs.fcitx5-mozc ];
+    fcitx5.waylandFrontend = true;
+    fcitx5.addons = with pkgs;[
+      fcitx5-mozc 
+      fcitx5-gtk 
+    ];
+  };
+  environment.sessionVariables = {
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+      GLFW_IM_MODULE = "ibus";
   };
 }
