ed1441f05cc4ba77458d1beff7337cc02f245ddb
ru322 <168622173+ru322@users.noreply.github.com>
2026-08-27 05:33:12 +0900

    sakura home fix

--- machines/Sakura/home.nix
+++ machines/Sakura/home.nix
@@ -10,8 +10,6 @@
   imports = [
     ../../common/home-mgr/neovim.nix
     ../../common/home-mgr/dev.nix
-    ../../common/home-mgr/desktop.nix
-    ../../common/home-mgr/onlyoffice.nix
     #../../common/home-mgr/sunshine.nix
     ../../common/home-mgr/wl-clipboard.nix
     ../../common/home-mgr/git.nix
@@ -22,12 +20,6 @@
     #../../common/home-mgr/nixos-vscoder-server.nix
     #../../common/home-mgr/docker-cli.nix
     ../../common/home-mgr/codex.nix
-    ../../common/home-mgr/chatgpt.nix
-    ../../common/home-mgr/waybar.nix
-    ../../common/home-mgr/discord.nix
-    ../../common/home-mgr/steam.nix
-    ../../common/home-mgr/obsidian.nix
-    ../../common/home-mgr/unity.nix
   ];
 
   # Hyprland config
