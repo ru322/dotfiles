1d83df5bf03a4f94fb3810e9758a6a7bfea5b858
ru322 <168622173+ru322@users.noreply.github.com>
2026-08-27 05:28:02 +0900

    sakura

--- flake.nix
+++ flake.nix
@@ -73,6 +73,18 @@
           ./common/home-mgr/ghostty.nix
         ];
       };
+      Sakura = inputs.home-manager.lib.homeManagerConfiguration {
+	pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./machines/Sakura/home.nix
+        ];
+      };
     };
     nixosConfigurations = {
       Nix = inputs.nixpkgs.lib.nixosSystem {
---
+++ machines/Sakura/home.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+{
+  home = rec {
+    username = "ru3";
+    homeDirectory = "/home/${username}";
+    stateVersion = "22.11";
+  };
+  programs.home-manager.enable = true;
+
+  imports = [
+    ../../common/home-mgr/neovim.nix
+    ../../common/home-mgr/dev.nix
+    ../../common/home-mgr/desktop.nix
+    ../../common/home-mgr/onlyoffice.nix
+    #../../common/home-mgr/sunshine.nix
+    ../../common/home-mgr/wl-clipboard.nix
+    ../../common/home-mgr/git.nix
+    ../../common/home-mgr/tmux.nix
+    ../../common/home-mgr/yazi.nix
+    ../../common/home-mgr/yazi-gvfs.nix
+    ../../common/home-mgr/zsh.nix
+    #../../common/home-mgr/nixos-vscoder-server.nix
+    #../../common/home-mgr/docker-cli.nix
+    ../../common/home-mgr/codex.nix
+    ../../common/home-mgr/chatgpt.nix
+    ../../common/home-mgr/waybar.nix
+    ../../common/home-mgr/discord.nix
+    ../../common/home-mgr/steam.nix
+    ../../common/home-mgr/obsidian.nix
+    ../../common/home-mgr/unity.nix
+  ];
+
+  # Hyprland config
+  #xdg.configFile."hypr" = {
+  #  source = ../../common/.config/hypr;
+  #  recursive = true;
+  #};
+
+  #home.packages = with pkgs; [
+  #  vscode
+  #];
+}
