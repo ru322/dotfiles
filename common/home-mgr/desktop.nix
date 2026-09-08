3f57a06f06bad29a5f2bea33bdfc92d3fac5d19a
ru322 <168622173+ru322@users.noreply.github.com>
2026-09-08 16:52:33 +0900

    fix: set Firefox as the default browser

--- common/home-mgr/desktop.nix
+++ common/home-mgr/desktop.nix
@@ -1,5 +1,15 @@
 { pkgs, ... }:
 {
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+    };
+  };
+
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
